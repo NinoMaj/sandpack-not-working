81225e5818c2913109b9ddcd638381e9291843d5
Nino Majder <nino.majder@gmail.com>
2020-06-30 18:09:01 +0200

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  FileExplorer,
+  CodeMirror,
+  BrowserPreview,
+  SandpackProvider,
+} from "react-smooshpack/es/components";
+import "react-smooshpack/dist/styles.css";
+
+const files = {
+  "/index.js": {
+    code: `\
+import React from "react";
+import { render } from "react-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      Working
+    </div>
+  );
+}
+
+render(<App />, document.getElementById("react-app"));`,
+  },
+  "/public/index.html": {
+    code: `\
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>App</title>
+</head>
+<body>
+  <noscript>You need to enable JavaScript to run this app.</noscript>
+  <div id="react-app"></div>
+</body>
+</html>
+`,
+  },
+};
+
+const dependencies = {
+  react: "16.13.1",
+  "react-dom": "16.13.1",
+};
+
+function App() {
+  return (
+    <div>
+      <SandpackProvider
+        files={files}
+        dependencies={dependencies}
+        entry="/index.js"
+        // bundlerURL="https://sandpack-0-0-61.codesandbox.io/" Uncomment to make it work
+        style={{
+          width: "100%",
+          maxWidth: 1800,
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            height: "100%",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <FileExplorer />
+
+          <CodeMirror
+            style={{
+              flex: 1,
+              overflowX: "hidden",
+            }}
+          />
+          <BrowserPreview
+            style={{
+              flex: 1,
+              overflowX: "hidden",
+            }}
+          />
+        </div>
+      </SandpackProvider>
     </div>
   );
 }
